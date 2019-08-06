ea9743b58f15fadc0863c938c0173789b40cf53e
Glen Warren <glenwarren@zoho.com>
2019-08-06 15:49:22 +0200

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # get 'tasks', to: 'tasks#index'
   # get 'tasks/new', to: 'tasks#new'
   # post 'tasks', to: 'tasks#create'
-  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id', to: 'tasks#show', as: :new_task
   # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   # patch 'tasks/:id', to: 'tasks#update'
-  # delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
   resources :tasks
 end
